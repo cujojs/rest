c555e0d00cc4774d20d56a9caff1adc49626c908
Scott Andrews <andrewss@vmware.com>
2013-02-22 14:30:34 -0500

    abort sauce tests for pull request builds

--- test/sauce-driver.js
+++ test/sauce-driver.js
@@ -69,6 +69,12 @@
 	travisJobNumber = process.env.TRAVIS_JOB_NUMBER || '';
 	travisCommit = process.env.TRAVIS_COMMIT || '';
 
+	if (travisCommit && !accessKey) {
+		// probably running as a pull request build
+		// without an access key we can't run sauce tests, give up
+		return;
+	}
+
 	passFailInterceptor = (function (username, password) {
 		var interceptor, basicAuth, mime;
 
